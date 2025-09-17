0dc1541a962fc294e3abd6db48dd7671e48add1a
Bl4ck5tar <b1nn3tw0rk@foxmail.com>
2025-09-17 21:27:34 +0800

    更新全局变量

--- server/global/global.go
+++ server/global/global.go
@@ -3,10 +3,18 @@ package global
 import (
 	"BinLog/server/config"
 
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/go-redis/redis"
+	"github.com/songzhibin97/gkit/cache/local_cache"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 var (
 	Config		*config.Config
 	Log 		*zap.Logger
+	DB			*gorm.DB
+	ESClient	*elasticsearch.TypedClient	//全局es客户端
+	Redis		redis.Client				//全局redis客户端
+	BlackCache	local_cache.Cache			//进程内的本地缓存
 )
