000be6fb350ec9ac713760819e74aef88b5d2cc8
Bl4ck5tar <b1nn3tw0rk@foxmail.com>
2025-09-20 22:04:36 +0800

    完成验证码基本功能

---
+++ server/model/other/es_index.go
@@ -0,0 +1,12 @@
+package other
+
+import "encoding/json"
+
+type Data struct {
+	ID 	*string			`json:"id"`
+	Doc json.RawMessage	`json:"doc"`
+}
+//ESIndexResponse ES 数据
+type ESIndexResponse struct {
+	Data []Data	`json:"data"`
+}
