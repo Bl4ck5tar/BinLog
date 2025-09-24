6eff5e8d64657bd3e7c915284611367627cb7c40
Bl4ck5tar <b1nn3tw0rk@foxmail.com>
2025-09-24 22:34:04 +0800

    请求参数

--- server/model/request/base.go
+++ server/model/request/base.go
@@ -1,6 +1,6 @@
 package request
 
-type sendEmailVerificationCode struct {
+type SendEmailVerificationCode struct {
 	Email 		string	`json:"email" binding:"required,email"`
 	Captcha 	string	`json:"captcha" binding:"required,len=6"`
 	CaptchaID 	string	`json:"captcha_id" binding:"required"`
