779bda188d7ecb1883916dcb6d24b94ba3d15a89
Bl4ck5tar <b1nn3tw0rk@foxmail.com>
2025-10-10 21:46:33 +0800

    文章浏览量更新

--- server/task/enter.go
+++ server/task/enter.go
@@ -1,7 +1,20 @@
 package task
 
-import "github.com/robfig/cron/v3"
+import (
+	"BinLog/server/global"
 
+	"github.com/robfig/cron/v3"
+	"go.uber.org/zap"
+)
+
+//每一个小时执行一次更新浏览量操作
 func RegisterScheduledTasks(c *cron.Cron) error {
+	if _, err := c.AddFunc("@hourly", func() {
+		if err := UpdateArticleViewsSyncTask(); err != nil {
+			global.Log.Error("Failed to update article views:", zap.Error(err))
+		}
+	}); err != nil {
+		return err
+	}
 	return nil
 }
